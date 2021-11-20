476ff840d025c81e4e4891d8b66181dcadcc3dca
Sergio E. Prellezo <sereprec@gmail.com>
2021-11-19 21:03:07 -0300

    Rutas get

--- routes/productos.js
+++ routes/productos.js
@@ -5,23 +5,30 @@ const productos = require("../model/productos");
 router.get("/", async (req, res) => {
   try {
     const lista = await productos.getAll();
-    res.send(lista);
+    res.json(lista);
   } catch (error) {
     console.log(error);
-    res.send({
+    res.status(500).json({
       error: "No se pudo recuperar la infomación"
     });
   }
 });
 router.get("/:id", async (req, res) => {
   try {
-    const lista = await productos.getAll();
-    res.send(lista);
+    let id = req.params.id;
+    if (isNaN(id)) res.json({ error: "El parámetro no es válido" });
+    else {
+      id = parseInt(id);
+      const producto = await productos.getById(id);
+      producto !== null
+        ? res.json(producto)
+        : res.json({ error: "Producto no encontrado" });
+    }
   } catch (error) {
-    // console.log(error);
-    // res.send({
-    //   error: "No se pudo recuperar la infomación"
-    // });
+    res.status(500).json({
+      error: "No se pudo recuperar la infomación"
+    });
+    console.log(error);
   }
 });
 
